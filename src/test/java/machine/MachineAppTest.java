a6b5f709c5e1528616e6e760ed4736ce9c98a868
Cyrille francois <cyrille.francois@ateno-tech.com>
2019-03-21 05:56:18 +0100

    First test

---
+++ src/test/java/machine/MachineAppTest.java
@@ -0,0 +1,15 @@
+package machine;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MachineAppTest {
+
+	@Test
+	public void test() {
+		MachineApp.main(new String[0]);
+		assertTrue(true);
+	}
+
+}
